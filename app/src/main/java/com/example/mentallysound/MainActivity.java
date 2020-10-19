761fa4ac69c11ca09deca724e3264cecdb8dc3e0
arjunprabhu2011 <arjunprabhu2011@gmail.com>
2020-10-19 17:18:36 -0500

    Create Home Page and Sign Up Page

--- app/src/main/java/com/example/mentallysound/MainActivity.java
+++ app/src/main/java/com/example/mentallysound/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mentallysound;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button createAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createAccount = (Button) findViewById(R.id.createAccountButton);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
