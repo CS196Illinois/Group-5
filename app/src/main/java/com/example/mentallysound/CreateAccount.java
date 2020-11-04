b988748ebd24cf3d9204c7e5314ccc986a806b19
Yiming Zhao <yiming14@illinois.edu>
2020-11-03 21:15:16 -0600

    Add Firebase Authentication to CreateAccount

--- app/src/main/java/com/example/mentallysound/CreateAccount.java
+++ app/src/main/java/com/example/mentallysound/CreateAccount.java
@@ -1,13 +1,21 @@
 package com.example.mentallysound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -15,6 +23,7 @@ import java.util.Map;
 
 public class CreateAccount extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     public Button initialSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,7 @@ public class CreateAccount extends AppCompatActivity {
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
                 //Create a Map of all the data of an user
                 Map<String, Object> user = new HashMap<>();
+                mAuth = FirebaseAuth.getInstance();
                 EditText inputName = (EditText) findViewById(R.id.textName);
                 EditText inputEmail = (EditText) findViewById(R.id.textEmail);
                 EditText inputPassword = (EditText) findViewById(R.id.textPassword);
@@ -42,6 +52,13 @@ public class CreateAccount extends AppCompatActivity {
                 user.put("name", name);
                 user.put("email", email);
                 user.put("password", password);
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccount.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(getApplicationContext(), "account registered", Toast.LENGTH_SHORT).show();;
+                    }
+                });
 
                 // Add a new document with a generated ID
                 db.collection("users").add(user);
