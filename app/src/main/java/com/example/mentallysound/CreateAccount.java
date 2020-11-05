db2beebb8430883836a0701dd5f6edadd08082c1
Prashant Naganaboyina <pnaganab@gmail.com>
2020-11-05 15:37:50 -0500

    added validation for confirm email

--- app/src/main/java/com/example/mentallysound/CreateAccount.java
+++ app/src/main/java/com/example/mentallysound/CreateAccount.java
@@ -4,19 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import android.graphics.Color;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.proto.TargetGlobal;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +28,7 @@ public class CreateAccount extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     public Button initialSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +46,41 @@ public class CreateAccount extends AppCompatActivity {
                 mAuth = FirebaseAuth.getInstance();
                 EditText inputName = (EditText) findViewById(R.id.textName);
                 EditText inputEmail = (EditText) findViewById(R.id.textEmail);
+                EditText inputConfirmEmail = (EditText) findViewById(R.id.editTextTextEmailAddress2);
                 EditText inputPassword = (EditText) findViewById(R.id.textPassword);
 
                 String name = inputName.getText().toString();
                 String email = inputEmail.getText().toString();
+                String confirmEmail = inputConfirmEmail.getText().toString();
                 String password = inputPassword.getText().toString();
 
+                //checking to see if the two emails inputted are the same
+                if (!(confirmEmail.equals(email))) {
+                    new SweetAlertDialog(CreateAccount.this, SweetAlertDialog.ERROR_TYPE)
+                            .setTitleText("Confirmation Email...")
+                            .setContentText("The two emails you inputted are not the same.")
+                            .show();
+                } else { //if the emails are the same continue
+
+                    user.put("id", 2);
+                    user.put("name", name);
+                    user.put("email", email);
+                    user.put("password", password);
+
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccount.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Toast.makeText(getApplicationContext(), "account registered", Toast.LENGTH_SHORT).show();;
+                        }
+                    });
+
+                    // Add a new document with a generated ID
+                    db.collection("users").add(user);
+                    Intent intent = new Intent (CreateAccount.this, QuestionsStart.class);
+                    startActivity(intent);
+                }
+
+                /*
                 user.put("id", 2);
                 user.put("name", name);
                 user.put("email", email);
@@ -64,6 +97,7 @@ public class CreateAccount extends AppCompatActivity {
                 db.collection("users").add(user);
                 Intent intent = new Intent (CreateAccount.this, QuestionsStart.class);
                 startActivity(intent);
+                 */
             }
         });
     }
